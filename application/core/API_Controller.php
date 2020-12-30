173796996853aaa16981b544384a7f2a07b569cf
Rizky Aditya Fahreza <rizkyraf88@gmail.com>
2020-12-31 02:47:04 +0700

    Add Auth

--- application/core/API_Controller.php
+++ application/core/API_Controller.php
@@ -11,6 +11,23 @@ class API_Controller extends REST_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+
+		$token = explode(" ", $this->input->get_request_header('Authorization'));
+
+		$jwtApi = JWT::decode(@$token[1], secretKey(), true);
+
+		if (@$jwtApi->logged AND time() < @$jwtApi->exp) {
+			$this->jwtData = $jwtApi;
+		}else{
+			$responseData = null;
+			$responseError = TRUE;
+			$responseCode = "XX00";
+			$responseDesc = 'Failed to authenticate token, exp';
+
+			$response =  resultJson($responseError, $responseCode, $responseDesc, $responseData);
+
+			$this->response($response, 200);
+		}
 	}
 }
 
